09a88f8e01ce28a497a10aa25018ed016ea2ca72
armaninyow <armaninyow@gmail.com>
2026-07-17 10:15:38 +0800

    Delete 1.21.2_1.21.4 directory

--- 1.21.2_1.21.4/src/main/java/com/armaninyow/morecloudlayers/render/CloudLayerAlphaAccessor.java
+++
@@ -1,6 +0,0 @@
-package com.armaninyow.morecloudlayers.render;
-
-// implemented by CloudRendererMixin - lets us override the alpha vanilla's render() hardcodes to 1.0F
-public interface CloudLayerAlphaAccessor {
-	void morecloudlayers$setAlpha(float alpha);
-}
